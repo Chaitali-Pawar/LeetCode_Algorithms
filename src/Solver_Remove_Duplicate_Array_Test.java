2d2ab2b4f9ade775cd8b89236da41a346d1acadd
Chaitali Pawar <chaitali.projectx@gmail.com>
2019-01-20 15:01:28 -0500

    add program to remove duplicates from program in O(1) memory space

---
+++ src/Solver_Remove_Duplicate_Array_Test.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+public class Solver_Remove_Duplicate_Array_Test {
+    @Test
+    public void test(){
+        Solve_Remove_Duplicates solve = new Solve_Remove_Duplicates();
+        int input_array[] ={1,1,2,3};
+        Assert.assertEquals();
+
+
+    }
+}
